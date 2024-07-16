cf1a3b47166769c1e6f5bc4f8751324d9e8e7667
jfb <jf.bouchet.tlse@gmail.com>
2024-07-16 14:46:06 +0200

    creation d'un fichier bash et son execution

---
+++ exemple.sh
@@ -0,0 +1,6 @@
+# afficher le texte dans le terminal
+echo "Choisir le monde de votre dossier"
+# demander une valeur à l'utilisateur
+read dossier
+# créer un dossier avec le nom du dossier
+mkdir $dossier
